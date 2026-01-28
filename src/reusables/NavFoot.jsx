efb0b8b1bdc546f7f78fb4fb8d35403eae2935e6
Ace Lawrence Zuñiga Clavano <acelawrence007@gmail.com>
2026-01-28 15:04:42 +0800

    Continue developing the footer
    - Add Swiper Carousel
    - Add the footer banner
    - Add View Google Maps button for the embedded map
    - Put repeating elements in objects for efficiency
    - Add placeholder Newsletter Field & Email Us Button
    
    --PENDING--
    
    - Build the gallery section of footer
    - Clean the explore Dasol carousel
    - Figure out how to blend the main pages w/ footer

--- src/reusables/NavFoot.jsx
+++ src/reusables/NavFoot.jsx
@@ -2,22 +2,28 @@ import { MdOutlineLocalPhone, MdOutlineEmail  } from "react-icons/md";
 import { FaInstagram } from "react-icons/fa";
 import { FiFacebook } from "react-icons/fi";
 
-import { Link } from "react-router-dom";
+import { href, Link } from "react-router-dom";
+
+import ExploreDasol from "./ExploreDasol";
 
 
 const NavFoot = () => {
     // Styling Variables
-    const foot_cont = "flex flex-col items-center px-[32px] py-[56px] w-full border-4";
-    const foot_content = "flex justify-between gap-[40px] w-full py-[40px] px-[72px]";
+    const foot_cont = "flex flex-col items-center px-[16px] py-[24px] w-full";
+    const foot_content = "flex flex-col justify-between gap-[40px] w-full py-[40px] px-[8px]";
 
 
     const locationContact = "flex items-center gap-[24px]";
-    const location_explore = "flex flex-col items-start gap-[40px]";
-    const location_info_cont = "flex flex-col items-start gap-[16px]";
+    const foot_loc_explore = "flex flex-col items-start gap-[40px]";
 
-    const location_info_content = "flex flex-col items-start gap-[16px]"
+    const foot_location_cont = "flex flex-col items-start gap-[16px]";
+    const foot_location_content = "flex flex-col items-start gap-[16px]"
     const foot_location_p = "w-[70%] text-main_text/60 text-location_p";
     const foot_contacts_p = "text-main_text/60 text-contacts_p";
+
+    const foot_explore_cont = "flex flex-col items-start gap-[8px] object-cover overflow-hidden relative";
+
+    const foot_map_cont = "flex flex-col gap-[24px]";
 
     const foot_display = "font-Libre text-primary";
     const foot_h3s = "font-Raleway font-medium text-foot_h3s";
@@ -27,129 +33,158 @@ const NavFoot = () => {
     const start_element_col = "flex flex-col items-start";
     const center_element_row = "flex items-center";
     const start_element_row = "flex items-start";
+    const between_element_row = "flex justify-between";
 
     // Map Iframe Resource
     const foot_map = {
-            title: "LasCalas Location",
-            src: "/lascalas_assets/Website/Map/map.png",
-            class: "w-[400px] h-[500px] rounded-[8px] hover:scale-105",
-        }
+        title: "LasCalas Location",
+        src: "/lascalas_assets/Website/Map/map.png",
+        class: "w-[400px] h-[500px] rounded-[8px] hover:scale-105",
+    }
+
+    const contact_info_map = [
+        "(+63) 917 707 3891",
+        "(+63) 2 8531 3008",
+    ]
+
+    const contact_links_map = [
+        {
+            id: 1,
+            iconPack: <FiFacebook size={24}/>,
+            label: "Facebook",
+            href: "https://www.facebook.com/lascalasresortluxuryestate/",
+            class: "hover:text-highlight",
+            target: "_blank",
+        },
+        {
+            id: 2,
+            iconPack: <FaInstagram size={24}/>,
+            label: "Instagram",
+            href: "",
+            class: "hover:text-highlight",
+            target: "_blank",
+        },
+        {
+            id: 3,
+            iconPack: <MdOutlineEmail size={24} />,
+            label: "info@lascalasresort.com",
+            href: "mailto:info@lascalasresort.com",
+            class: "hover:text-highlight",
+            target: "_blank",
+        },
+        {
+            id: 4,
+            iconPack: <MdOutlineLocalPhone size={24} />,
+            label: "(+63) 917 707 3891",
+            href: "tel:+639177073891",
+            class: "hover:text-highlight",
+            target: "_blank",
+        },
+        {
+            id: 5,
+            iconPack: <MdOutlineLocalPhone size={24} />,
+            label: "(+63) 2 8531 3008",
+            href: "tel:+63285313008",
+            class: "hover:text-highlight",
+            target: "_blank",
+        },
+    ]
 
     return (
-        <div className={foot_cont}>
+        <div className={`${foot_cont}`}>
 
-            <div className={foot_content}>
+            <div className={`${foot_content}`}>
 
-                <div className={location_explore}>
-                    <div className={location_info_cont}>
-                        <h1 className="font-Raleway text-[32px]">Our Location</h1>
+                <div className={`${between_element_row} px-[32px]`}>
+                    <div className={foot_loc_explore}>
+                        <div className={foot_location_cont}>
+                            <h1 className="font-Raleway text-[32px]">Our Location</h1>
+                            <div className={foot_location_content}>
+                                <p className={foot_location_p}><span className="text-accent">LasCalas Resort and Luxury Estate</span>, Barangay Osmena, Dasol, 2411 Pangasinan</p>
+                    
+                                {/* Contact Number - Information */}
+                                {contact_info_map.map((number, index) => (
+                                    <div key={index} className="flex flex-col items-start gap-[8px]">
+                                        <div className={locationContact}>
+                                            <MdOutlineLocalPhone size={24} className="text-main_text/60"/>
+                                            <p className={foot_contacts_p}>{number}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        <div className={`${foot_explore_cont}`}>
+                            <h1 className="font-Raleway text-[32px]">Explore <span className="hover:text-highlight hover:font-semibold hover:cursor-pointer">Dasol</span></h1>
+                            <ExploreDasol />
+                        </div>
+                    </div>
 
-                        <div className={location_info_content}>
-                            <p className={foot_location_p}><span className="text-accent">LasCalas Resort and Luxury Estate</span>, Barangay Osmena, Dasol, 2411 Pangasinan</p>
-                            
-                            <div className="flex flex-col items-start gap-[8px]">
-                                <div className={locationContact}>
-                                    <MdOutlineLocalPhone size={24} className="text-main_text/60"/>
-                                    <p className={foot_contacts_p}>(+63) 917 707 3891</p>
+                    <div className={`${foot_map_cont}`}>
+                        <div className="flex items-center justify-between">
+                            <h1 className="font-Raleway text-[32px]">Map</h1>
+                            <a href="https://maps.app.goo.gl/1gqmxGKsbwLfq4bo9" className="font-Montserrat text-accent hover:text-highlight hover:underline" target="_blank">View On Google Maps</a>
+                        </div>
+                        <img
+                            src={foot_map.src}
+                            title={foot_map.title}
+                            className={foot_map.class}
+                        ></img>
+                    </div>
+                </div>
+
+                <div className={``}></div>
+
+                <div className="flex flex-col items-center gap-24">
+                    <div className={`${center_element_row} gap-24 px-[48px]`}>
+                        <div className={`${center_element_col} gap-[4px] w-[30%]`}>
+                            <img src="/lascalas_assets/lascalasBlue.png" alt="LasCalas Logo Blue" className="w-[120px]" />
+                            <div className={`${center_element_col}`}>
+                                <h1 className={`${foot_display} text-foot_display`}>LasCalas</h1>
+                                <h2 className={`${foot_display} text-foot_display2`}>Resort & LUXURY ESTATE</h2>
+                            </div>
+                            <p className={`${foot_display} text-foot_display3`}><span className="text-main_text">Indulge in</span> Serenity | <span className="text-main_text">Discover Your Sanctuary of</span> Luxury</p>
+                        </div>
+                        <div className={`${start_element_row} gap-24`}>
+                            <div className={`${start_element_col} gap-8`}>
+                                <h3 className={`${foot_h3s}`}>Navigation</h3>
+                                <div className={`${start_element_col} gap-[16px]`}>
+                                    <Link to="/packages" className={`font-Montserrat text-foot_body_links hover:text-highlight`}>Packages</Link>
+                                    <Link to="/explore" className={`font-Montserrat text-foot_body_links hover:text-highlight`}>Explore</Link>
+                                    <Link to="/celeb" className={`font-Montserrat text-foot_body_links hover:text-highlight`}>Celebrations</Link>
                                 </div>
-                                <div className={locationContact}>
-                                    <MdOutlineLocalPhone size={24} className="text-main_text/60"/>
-                                    <p className={foot_contacts_p}>(+63) 2 8531 3008</p>
+                            </div>
+                            <div className={`${start_element_col} gap-8`}>
+                                <h3 className={`${foot_h3s}`}>Contact Information</h3>
+                                <div className={`${start_element_col} gap-[16px]`}>
+                                    {contact_links_map.map((item) => (
+                                        <div className={`${center_element_row} gap-[8px] ${item.class}`}>
+                                            {item.iconPack}
+                                            <a href={item.href} className="font-Nunito text-foot_body_links">{item.label}</a>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className={`${start_element_col} gap-[32px]`}>
+                                <div className={`${start_element_col} gap-[8px]`}>
+                                    <h3 className={`${foot_h3s}`}>Newsletter</h3>
+                    
+                                    <div>
+                                        <img src="/lascalas_assets/Website/Placeholders/Placeholders1.png" alt="" className="w-[360px]"/>
+                                    </div>
+                                </div>
+                    
+                                <div className={`${start_element_col} gap-[8px]`}>
+                                    <h3 className={`${foot_h3s}`}>Email Us</h3>
+                    
+                                    <div>
+                                        <img src="public/lascalas_assets/Website/Placeholders/Placeholders2.png" alt="" className="w-[360px]"/>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="w-full overflow-hidden relative">
-                        <h1 className="font-Raleway text-[32px]">Explore <span className="hover:text-highlight hover:font-semibold hover:cursor-pointer">Dasol</span></h1>
-                        {/* <ExploreCarousel />  */}
-                    </div>
-                </div>
 
-                <div className="flex flex-col gap-[24px]">
-                    <div className="flex items-center justify-between">
-                        <h1 className="font-Raleway text-[32px]">Map</h1>
-                        <a href="https://maps.app.goo.gl/1gqmxGKsbwLfq4bo9" className="font-Montserrat text-accent hover:text-highlight hover:underline" target="_blank">View On Google Maps</a>
-                    </div>
-                    <img 
-                        src={foot_map.src} 
-                        title={foot_map.title}
-                        className={foot_map.class}
-                    ></img>
-                </div>
-            </div>
-
-            <div></div>
-
-            <div className={`${center_element_row} gap-40`}>
-                <div className={`${center_element_col} gap-[4px]`}>
-                    <img src="/lascalas_assets/lascalasBlue.png" alt="LasCalas Logo Blue" className="w-[120px]" />
-                    <div className={`${center_element_col}`}>
-                        <h1 className={`${foot_display} text-foot_display`}>LasCalas</h1>
-                        <h2 className={`${foot_display} text-foot_display2`}>Resort & LUXURY ESTATE</h2>
-                    </div>
-
-                    <p className={`${foot_display} text-foot_display3`}><span className="text-main_text">Indulge in</span> Serenity | <span className="text-main_text">Discover Your Sanctuary of</span> Luxury</p>
-                </div>
-
-                <div className={`${start_element_row} gap-24`}>
-                    <div className={`${start_element_col} gap-8`}>
-                        <h3 className={`${foot_h3s}`}>Navigation</h3>
-
-                        <div className={`${start_element_col} gap-[16px]`}>
-                            <Link to="/packages" className={`font-Montserrat text-foot_body_links`}>Packages</Link>
-                            <Link to="/explore" className={`font-Montserrat text-foot_body_links`}>Explore</Link>
-                            <Link to="/celeb" className={`font-Montserrat text-foot_body_links`}>Celebrations</Link>
-                        </div>
-                    </div>
-
-                    <div className={`${start_element_col} gap-8`}>
-                        <h3 className={`${foot_h3s}`}>Contact Information</h3>
-
-                        <div className={`${start_element_col} gap-[16px]`}>
-                            <div className={`${center_element_row} gap-[8px]`}>
-                                <FiFacebook size={24}/>
-                                <a href="" className="font-Nunito text-foot_body_links">Facebook</a>
-                            </div>
-
-                            <div className={`${center_element_row} gap-[8px]`}>
-                                <FaInstagram size={24}/>
-                                <a href="" className="font-Nunito text-foot_body_links">Instagram</a>
-                            </div>
-
-                            <div className={`${center_element_row} gap-[8px]`}>
-                                <MdOutlineEmail  size={24}/>
-                                <a href="" className="font-Nunito text-foot_body_links">lascalasresort@gmail.com</a>
-                            </div>
-
-                            <div className={`${center_element_row} gap-[8px]`}>
-                                <MdOutlineLocalPhone size={24}/>
-                                <a href="" className="font-Nunito text-foot_body_links">(+63) 917 707 3891</a>
-                            </div>
-                            
-                            <div className={`${center_element_row} gap-[8px]`}>
-                                <MdOutlineLocalPhone size={24}/>
-                                <a href="" className="font-Nunito text-foot_body_links">(+63) 2 8531 3008</a>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={`${start_element_col} gap-[32px]`}>
-                        <div className={`${start_element_col} gap-[8px]`}>
-                            <h3 className={`${foot_h3s}`}>Newsletter</h3>
-                            
-                            <div>
-                                <img src="/lascalas_assets/Website/Placeholders/Placeholders1.png" alt="" className="w-[360px]"/>
-                            </div>
-                        </div>
-                        
-                        <div className={`${start_element_col} gap-[8px]`}>
-                            <h3 className={`${foot_h3s}`}>Email Us</h3>
-                            
-                            <div>
-                                <img src="public/lascalas_assets/Website/Placeholders/Placeholders2.png" alt="" className="w-[360px]"/>
-                            </div>
-                        </div>
-                    </div>
+                    <p>© 2026 LasCalas Resort. Crafted with ♥ in Dasol, Pangasinan.</p>
                 </div>
             </div>
 
