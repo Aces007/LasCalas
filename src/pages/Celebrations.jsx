3727d09499ab244b2cea355c667d55cd1e2dc8a5
Ace Lawrence Zuñiga Clavano <acelawrence007@gmail.com>
2026-01-30 15:05:16 +0800

    establish slideshows in other pages

--- src/pages/Celebrations.jsx
+++ src/pages/Celebrations.jsx
@@ -1,7 +1,19 @@
+import MainSlides from "../reusables/MainSlides";
+
 const Celebrations = () => {
+
+    const celebrations_slideshow = [
+        { id: 1, src: "/lascalas_assets/Website/Slideshows/Celebrations_Slideshow/Celebrations1.png" },
+        { id: 2, src: "/lascalas_assets/Website/Slideshows/Celebrations_Slideshow/Celebrations2.png" },
+        { id: 3, src: "/lascalas_assets/Website/Slideshows/Celebrations_Slideshow/Celebrations3.png" },
+        { id: 4, src: "/lascalas_assets/Website/Slideshows/Celebrations_Slideshow/Celebrations4.png" },
+        { id: 5, src: "/lascalas_assets/Website/Slideshows/Celebrations_Slideshow/Celebrations5.png" },
+    ]
+
     return (
         <div>
-            
+            <MainSlides pageTitle="Celebrations" slideContent={celebrations_slideshow} />
+
         </div>
     )
 }
