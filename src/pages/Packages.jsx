9d91dee773efa00bf6f272eb79d2ef25f663c1da
Ace Lawrence Zuñiga Clavano <acelawrence007@gmail.com>
2026-01-30 14:57:27 +0800

    fixed bugs and changed tab name

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -3,13 +3,13 @@ import MainSlides from "../reusables/MainSlides";
 const Packages = () => {
 
     const packages_slideshow = [
-        { id: 1, src: "public/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider1.png" },
-        { id: 2, src: "public/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider2.png" },
-        { id: 3, src: "public/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider3.png" },
-        { id: 4, src: "public/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider4.png" },
-        { id: 5, src: "public/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider5.png" },
-        { id: 6, src: "public/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider6.png" },
-        { id: 7, src: "public/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider7.png" },
+        { id: 1, src: "/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider1.png" },
+        { id: 2, src: "/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider2.png" },
+        { id: 3, src: "/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider3.png" },
+        { id: 4, src: "/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider4.png" },
+        { id: 5, src: "/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider5.png" },
+        { id: 6, src: "/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider6.png" },
+        { id: 7, src: "/lascalas_assets/Website/Slideshows/Packages_Slideshow/Pack_Slider7.png" },
     ]
 
     return (
