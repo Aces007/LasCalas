bbe8a6fa2da300dad52148366946b63bd9542275
Aces007 <acelawrence007@gmail.com>
2026-03-11 14:25:41 +0800

    fixing deployment issues

--- src/RouteFlow.jsx
+++ src/RouteFlow.jsx
@@ -1,5 +1,5 @@
 import {Routes, Route, useLocation} from "react-router-dom";
-import { AnimatePresence } from "framer-motion"; // controller that tracks when components enter (mounts or appears) or leave (unmmounts or disappears) the React tree
+import { AnimatePresence } from "framer-motion";
 
 import Hero from "./pages/Hero";
 import About from "./pages/About";
@@ -11,11 +11,11 @@ import WaterAdv from "./pages/subpages/explore/WaterAdv";
 import BuggyAdv from "./pages/subpages/explore/BuggyAdv";
 import RopeCourse from "./pages/subpages/explore/RopeCourse";
 import Celebrations from "./pages/Celebrations";
-import EventPavilion from "./pages/subpages/celebrations/EventPavilion";
-import Pergola from "./pages/subpages/celebrations/Pergola";
-import GamingPool from "./pages/subpages/celebrations/GamingPool";
-import CampingGround from "./pages/subpages/celebrations/CampingGround";
-import Gazebo from "./pages/subpages/celebrations/Gazebo";
+import EventPavilion from "./pages/subpages/celebration_showcase/EventPavilion";
+import Pergola from "./pages/subpages/celebration_showcase/Pergola";
+import GamingPool from "./pages/subpages/celebration_showcase/GamingPool";
+import CampingGround from "./pages/subpages/celebration_showcase/CampingGround";
+import Gazebo from "./pages/subpages/celebration_showcase/Gazebo";
 
 export default function RouteFlow () {
     const location = useLocation();
@@ -23,22 +23,21 @@ export default function RouteFlow () {
     return (
         <AnimatePresence>
             <Routes location={location} key={location.pathname}>
-                <Route path="/" element={<Hero />}/>
-                <Route path="/about" element={<About />}/>
-                <Route path="/packagesmenu" element={<PackagesMenu />}/>
-                <Route path="/packagedetails/:id" element={<PackageDetails />}/>
-                <Route path="/explore" element={<Explore />}/>
-                <Route path="/exploreATV" element={<ATVAdv />}/>
-                <Route path="/exploreWater" element={<WaterAdv />}/>
-                <Route path="/exploreBug" element={<BuggyAdv />}/>
-                <Route path="/exploreRope" element={<RopeCourse />}/>
-                <Route path="/celeb" element={<Celebrations />}/>
-                <Route path="/eventPav" element={<EventPavilion />}/>
-                <Route path="/pergola" element={<Pergola />}/>
-                <Route path="/gamPool" element={<GamingPool />}/>
-                <Route path="/campGround" element={<CampingGround />}/>
-                <Route path="/gaze" element={<Gazebo />}/>
-
+                <Route path="/" element={<Hero />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/packagesmenu" element={<PackagesMenu />} />
+                <Route path="/packagedetails/:id" element={<PackageDetails />} />
+                <Route path="/explore" element={<Explore />} />
+                <Route path="/exploreATV" element={<ATVAdv />} />
+                <Route path="/exploreWater" element={<WaterAdv />} />
+                <Route path="/exploreBug" element={<BuggyAdv />} />
+                <Route path="/exploreRope" element={<RopeCourse />} />
+                <Route path="/celebrations" element={<Celebrations />} />
+                <Route path="/eventPavilion" element={<EventPavilion />} />
+                <Route path="/pergola" element={<Pergola />} />
+                <Route path="/gamingPool" element={<GamingPool />} />
+                <Route path="/campingGround" element={<CampingGround />} />
+                <Route path="/gazebo" element={<Gazebo />} />
             </Routes>
         </AnimatePresence>
     );
