f049c78cdf229e940053726a0139bdd6bd180b67
Ace Lawrence Zuñiga Clavano <acelawrence007@gmail.com>
2026-05-22 08:20:09 +0800

    minor changes in hovering effects

--- src/pages/Amenities.jsx
+++ src/pages/Amenities.jsx
@@ -13,7 +13,7 @@ const Amenities = () => {
         const main_page_content_cont = "relative group w-full h-[300px] resSm:h-[250px] resMd:h-[350px] resLg:h-[400px] overflow-hidden rounded-lg";
         const main_page_cards_img = "w-full h-full object-cover transition-transform duration-700 group-hover:scale-110";
         const main_page_content_name_cont = "absolute bottom-0 left-0 w-full p-6 flex justify-center group-hover:opacity-0";
-        const main_page_desc_cont = "absolute inset-0 left-0 bg-cards_overlay/80 opacity-0 flex flex-col items-center justify-center gap-[40px] group-hover:opacity-100 transition-opacity duration-800 ease-in-out";
+        const main_page_desc_cont = "absolute inset-0 left-0 bg-cards_overlay/80 opacity-0 flex flex-col items-center justify-center gap-[40px] group-hover:opacity-100 transition-opacity duration-800 ease-in-out hover:cursor-pointer";
 
         //--TEXTS--//
         const main_page_content_name = "font-Montserrat uppercase text-center text-background_lightTxt font-semibold tracking-[5%] resSm:text-[14px] resMd:text-[15px] resLg:text-[17px]";
--- src/pages/Celebrations.jsx
+++ src/pages/Celebrations.jsx
@@ -10,7 +10,7 @@ const Celebrations = () => {
         const main_page_content_cont = "relative group w-full h-[300px] resSm:h-[250px] resMd:h-[350px] resLg:h-[400px] overflow-hidden rounded-lg";
         const main_page_cards_img = "w-full h-full object-cover transition-transform duration-700 group-hover:scale-110";
         const main_page_content_name_cont = "absolute bottom-0 left-0 w-full p-6 flex justify-center group-hover:opacity-0";
-        const main_page_desc_cont = "absolute inset-0 left-0 bg-cards_overlay/80 opacity-0 flex flex-col items-center justify-center gap-[40px] group-hover:opacity-100 transition-opacity duration-800 ease-in-out";
+        const main_page_desc_cont = "absolute inset-0 left-0 bg-cards_overlay/80 opacity-0 flex flex-col items-center justify-center gap-[40px] group-hover:opacity-100 transition-opacity duration-800 ease-in-out hover:cursor-pointer";
         
         //--TEXTS--//
         const packages_h2 = "font-Nunito font-bold w-[50%] text-center text-main_text/80 resSm:text-[14px] resSm:w-[90%]";
@@ -42,7 +42,7 @@ const Celebrations = () => {
 
      const celebrations_menu = [
         { id: 1, celebration_name: "Event Pavilion", src: "/lascalas_assets/Website/MainPages_Content/Celebrations/Celebrations1.webp", description: "Host the grandest celebration in the North. Our 800 sqm Pavilion is the region's only fully air-conditioned beachfront venue. Capable of hosting up to 500 guests, it offers a majestic setting where grand events meet the serenity of the sea.", link: "/eventPavilion" },
-        { id: 2, celebration_name: "Beach Pergola", src: "/lascalas_assets/Website/MainPages_Content/Celebrations/Celebrations2.webp", description: "Al fresco luxury. Our beautifully designed Beach Pergola offers a chic, open-air sanctuary where shaded comfort meets the sound of the waves—the ultimate spot for scenic dining and seaside relaxation.", link: "/pergola" },
+        { id: 2, celebration_name: "Beach Pergola", src: "/lascalas_assets/Website/MainPages_Content/Celebrations/Celebrations2.webp", description: "Al fresco luxury. Our beautifully designed Beach Pergola offers a chic, open-air sanctuary where shaded comfort meets the sound of the waves, the ultimate spot for scenic dining and seaside relaxation.", link: "/pergola" },
         { id: 3, celebration_name: "Gaming Villa & Infinity Pool", src: "/lascalas_assets/Website/MainPages_Content/Celebrations/Celebrations3.webp", description: "Play, party, and plunge. Elevate your gathering in our luxurious Gaming Villa, fully equipped with arcade games and billiards, just steps away from the serene Infinity Pool. It’s the ultimate entertainment hub for weddings, reunions, and lively celebrations.", link: "/gamingPool" },
         { id: 4, celebration_name: "Gazebo", src: "/lascalas_assets/Website/MainPages_Content/Celebrations/Celebrations6.webp", description: "A vow with a view. The Las Calas Gazebo provides an intimate, romantic backdrop for your ceremony. Perfectly positioned near the Event Tent, it ensures a seamless flow from your heartfelt 'I do' to your grand reception.", link: "/gazebo" },
         // { id: 5, celebration_name: "Camping Ground", src: "/lascalas_assets/Website/MainPages_Content/Celebrations/Celebrations4.webp", description: "Under the stars, by the sea. Gather around a crackling bonfire at the Las Calas Camping Grounds. Whether you're sipping wine or sharing stories, the cool ocean breeze and open skies create the perfect night to remember.", link: "/campingGround" },
